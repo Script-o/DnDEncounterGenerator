49f5a0e12ecc783553367b722a0a8b3ea7a4c1a3
Jon Shircliffe <jonshircliffe@gmail.com>
2025-09-21 19:58:47 -0400

    Commiting these changes? Seems to be some sort of issue with the debug info?

--- DnDEncounterGenerator.Console/obj/Debug/net8.0/DnDEncounterGenerator.Console.AssemblyInfo.cs
+++ DnDEncounterGenerator.Console/obj/Debug/net8.0/DnDEncounterGenerator.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DnDEncounterGenerator.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19a42c0ac58f5ba8f333bd38257aca518eaa160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f707f5d7207c4ff60419e58bd17a98a1cc99b00")]
 [assembly: System.Reflection.AssemblyProductAttribute("DnDEncounterGenerator.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DnDEncounterGenerator.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
